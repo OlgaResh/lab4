5f57613010f79e3f10388b500ef13644215afc11
OlgaResh <114303017+OlgaResh@users.noreply.github.com>
2023-01-10 21:14:40 +0200

    Add files via upload

---
+++ lab4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	char quote[128] = "";
+
+	cout << "Ваша любимая цитата из мультфильма";
+	cin >> quote;
+	cout << quote << endl;
+
+	return 0;
+}
